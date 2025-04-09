6a44081b0b4804a88888b1849da2dad7e97d1dae
jyzuz09 <alex_cena2000@hotmail.com>
2025-04-08 21:32:44 -0600

    version de prueba Railway

---
+++ src/main/java/com/example/ProyectoEmpresarial/ProyectoEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoEmpresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEmpresarialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoEmpresarial/ProyectoEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoEmpresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
